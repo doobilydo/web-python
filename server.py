df7e13f73e3ba7f51cd3585d795b9c6eccf3a55c
The13thDoc <thethneedinglorax@gmail.com>
2014-05-18 22:08:36 -0500

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('{{ site.baseurl }}/')
 def index():
-  return render_template('template.html')
+  return render_template('index.html')
 
 @app.route('{{ site.baseurl }}/my-link/')
 def my_link():
