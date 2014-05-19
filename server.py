c261a5329c8fecc8be0d8759e73794d22cc279f1
The13thDoc <thethneedinglorax@gmail.com>
2014-05-18 22:59:59 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
 ---
 title: Python Sever Code
 ---
+url = 'https://the13thdoc.github.io/web-python/'
+url = '{{ site.baseurl }}/'
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('{{ site.baseurl }}/')
+@app.route(url)
 def index():
   return render_template('index.html')
 
-@app.route('{{ site.baseurl }}/my-link/')
+@app.route(url + '/my-link/')
 def my_link():
   print 'I got clicked!'
 
